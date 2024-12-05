0936321bacda6a519f9e9ffc50a83b76b561b349
CadenTate <cadentate0511@gmail.com>
2024-12-05 16:57:01 -0600

    Part 1 Done!

--- Day 4/P1.py
+++ Day 4/P1.py
@@ -1,99 +1,138 @@
-path = r"C:\Users\Intern\Desktop\Advent-of-Code-2024\Day 4\input.txt"
+path = r"C:\Users\Caden\Desktop\Code\Advent-of-Code-2024\Day 4\input.txt"
 
 rawFile = open(path, 'r').readlines()
 
 size = len(rawFile)
 
-file = ""
+file = {}
 
-for line in rawFile:
-    file += line.strip()
+for i in range(len(rawFile)):
+    file[i] = rawFile[i].strip()
 
-# Functions checking forwards and backwards
-def checkForwards(i:int) -> bool:
-    if i > size*size - 4:
+# Functions checking North and South
+def checkNorth(y:int, x:int) -> bool:
+    if y < 3:
         return False
-    if file[i+1] != 'M':
+    if file[y-1][x] != 'M':
         return False
-    if file[i+2] != 'A':
+    if file[y-2][x] != 'A':
         return False
-    if file[i+3] != 'S':
+    if file[y-3][x] != 'S':
         return False
     return True
 
-def checkBackwards(i:int) -> bool:
-    if i < 4:
+def checkSouth(y:int, x:int) -> bool:
+    if y > size - 4:
         return False
-    if file[i-1] != 'M':
+    if file[y+1][x] != 'M':
         return False
-    if file[i-2] != 'A':
+    if file[y+2][x] != 'A':
         return False
-    if file[i-3] != 'S':
-        return False
-    return True
-
-# Functions checking up and down
-def checkUp(i:int) -> bool:
-    if i < size*size - size*(size-3):
-        return False
-    if file[i-size*1] != 'M':
-        return False
-    if file[i-size*2] != 'A':
-        return False
-    if file[i-size*3] != 'S':
+    if file[y+3][x] != 'S':
         return False
     return True
 
-def checkDown(i:int) -> bool:
-    if i >= size*size - size*(size-3):
+# Functions checking East and West
+def checkEast(y:int, x:int) -> bool:
+    if x < 3:
         return False
-    if file[i+size*1] != 'M':
+    if file[y][x-1] != 'M':
         return False
-    if file[i+size*2] != 'A':
+    if file[y][x-2] != 'A':
         return False
-    if file[i+size*3] != 'S':
+    if file[y][x-3] != 'S':
+        return False
+    return True
+
+def checkWest(y:int, x:int) -> bool:
+    if x > len(file[y]) - 4:
+        return False
+    if file[y][x+1] != 'M':
+        return False
+    if file[y][x+2] != 'A':
+        return False
+    if file[y][x+3] != 'S':
         return False
     return True
 
 # Functions for checking angles
-def checkNE(i:int) -> bool:
-    if i < (size * 3) + 3:
+def checkNE(y:int, x:int) -> bool:
+    if y < 3 or x < 3:
         return False
-    if file[i+size*1-1] != 'M':
+    if file[y-1][x-1] != 'M':
         return False
-    if file[i+size*2-2] != 'A':
+    if file[y-2][x-2] != 'A':
         return False
-    if file[i+size*3-3] != 'S':
+    if file[y-3][x-3] != 'S':
         return False
     return True
-        
 
-# Wrapper Functions
-def checkHorizontal(i:int) -> bool:
-    return checkForwards(i) or checkBackwards(i)
+def checkNW(y:int, x:int) -> bool:
+    if y < 3 or x > len(file[y]) - 4:
+        return False
+    if file[y-1][x+1] != 'M':
+        return False
+    if file[y-2][x+2] != 'A':
+        return False
+    if file[y-3][x+3] != 'S':
+        return False
+    return True
 
-def checkVertical(i:int) -> bool:
-    return checkUp(i) or checkDown(i)
+def checkSE(y:int, x:int) -> bool:
+    if y > size - 4 or x > len(file[y]) - 4 :
+        return False
+    if file[y+1][x+1] != 'M':
+        return False
+    if file[y+2][x+2] != 'A':
+        return False
+    if file[y+3][x+3] != 'S':
+        return False
+    return True
 
-def checkAngles(i:int) -> bool:
-    return checkNE(i)
+def checkSW(y:int, x:int) -> bool:
+    if y > size - 4 or x < 3:
+        return False
+    if file[y+1][x-1] != 'M':
+        return False
+    if file[y+2][x-2] != 'A':
+        return False
+    if file[y+3][x-3] != 'S':
+        return False
+    return True
 
 total = 0
 
-for i in range(len(file)):
-    # print(i,file[i])
-    # check if start
-    if file[i] != 'X':
-        continue
+for y in range(len(file)):
+    for x in range(len(file[y])):
+        # print(i,file[i])
+        # check if start
+        if file[y][x] != 'X':
+            continue
+        
 
-    if checkHorizontal(i):
-        # print(i,"H")
-        total += 1
-    if checkVertical(i):
-        # print(i,"V")
-        total += 1
-    if checkAngles(i):
-        total += 1
-        continue
+        if checkNorth(y,x):
+            print(y,x,"N")
+            total += 1
+        if checkSouth(y,x):
+            print(y,x,"S")
+            total += 1
+        if checkEast(y,x):
+            print(y,x,"E")
+            total += 1
+        if checkWest(y,x):
+            print(y,x,"W")
+            total += 1
+        if checkNE(y,x):
+            print(y,x,"NE")
+            total += 1
+        if checkNW(y,x):
+            print(y,x,"NW")
+            total += 1
+        if checkSE(y,x):
+            print(y,x,"SE")
+            total += 1
+        if checkSW(y,x):
+            print(y,x,"SW")
+            total += 1
 
 print(total)
